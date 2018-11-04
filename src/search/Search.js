425e085bc4ba607397de65e08cc460b6c7516ef3
Morgan Newman <mpnewman95@gmail.com>
2018-11-04 06:27:34 -0800

    Adds textarea to submit content

--- src/search/Search.js
+++ src/search/Search.js
@@ -5,22 +5,46 @@ import Footer from "../common/Footer";
 import sendContent from "../api";
 
 class Search extends Component {
+  state = {
+    submitting: false,
+    result: null
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     const content = this.text.value;
-    console.log(content);
-    sendContent(content);
-    this.text.value = "";
+    this.setState({ submitting: true }, () => {
+      this.text.value = "";
+      sendContent(content).then(res => {
+        console.log("response:", res);
+        this.setState({ result: res });
+      });
+    });
   };
 
   render() {
     return (
       <>
         <Nav />
-        <form onSubmit={this.handleSubmit}>
-          <textarea ref={text => (this.text = text)} />
-          <button type="submit">Submit</button>
-        </form>
+        <main className="main">
+          <div className="search-form-container">
+            <form className="search-form" onSubmit={this.handleSubmit}>
+              <div className="search-container">
+                <textarea
+                  className="search-input"
+                  ref={text => (this.text = text)}
+                />
+                <button
+                  className="search-submit"
+                  type="submit"
+                  disabled={this.state.submitting ? true : false}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </main>
         <Footer />
       </>
     );
